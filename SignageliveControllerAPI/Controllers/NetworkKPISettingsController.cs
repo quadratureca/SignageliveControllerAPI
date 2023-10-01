466945d6d1720f409106fdea35043f0bbe4d7be4
Phil Garrett <phil.garrett@outlook.com>
2023-09-30 22:15:35 -0400

    Check in

---
+++ SignageliveControllerAPI/Controllers/NetworkKPISettingsController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using System.Net;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SignageliveControllerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NetworkKPISettingsController : ControllerBase
+    {
+        string networkId;
+        string networkUrl;
+
+        public NetworkKPISettingsController()
+        {
+            IParameters p = new Parameters();
+            Parameters pp = p.GetParameters();
+            networkId = pp.NetworkId;
+            networkUrl = pp.NetWorkUrl;
+        }
+        
+        // GET: api/<NetworkKPISettingsController>
+        [HttpGet]
+        public string Get([FromHeader] string authorization)
+        {
+            RestClient restClient = new RestClient(networkUrl);
+
+            string request_resource = string.Format("networks/{0}/NetworkKPISettings", networkId);
+
+            RestRequest restRequest = new RestRequest(request_resource, Method.Get);
+            restRequest.AddHeader("Authorization", authorization);
+            restRequest.AddHeader("Content-Type", "application/json");
+
+            RestResponse response = restClient.Execute(restRequest);
+            if (response.IsSuccessful && response.Content != null)
+            {
+                return response.Content;
+            }
+            return "{}";
+        }
+
+        // GET api/<NetworkKPISettingsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<NetworkKPISettingsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<NetworkKPISettingsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<NetworkKPISettingsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SignageliveControllerAPI/Controllers/NetworksController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SignageliveControllerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NetworksController : ControllerBase
+    {
+        string networkId;
+        string networkUrl;
+
+        public NetworksController()
+        {
+            IParameters p = new Parameters();
+            Parameters pp = p.GetParameters();
+            networkId = pp.NetworkId;
+            networkUrl = pp.NetWorkUrl;
+        }
+
+        // GET: api/<NetworksController>
+        [HttpGet]
+        public string Get([FromHeader] string authorization)
+        {
+            RestClient restClient = new RestClient(networkUrl);
+
+            string request_resource = string.Format("networks/{0}", networkId);
+
+            RestRequest restRequest = new RestRequest(request_resource, Method.Get);
+            restRequest.AddHeader("Authorization", authorization);
+            restRequest.AddHeader("Content-Type", "application/json");
+
+            RestResponse response = restClient.Execute(restRequest);
+            if (response.IsSuccessful && response.Content != null)
+            {
+                return response.Content;
+            }
+            return "{}";
+        }
+
+        // POST api/<NetworksController>
+        [HttpPost]
+        public string Post([FromHeader] string authorization, [FromBody] string networkRequestObject)
+        {
+            RestClient restClient = new RestClient(networkUrl);
+
+            string request_resource = string.Format("networks/");
+
+            RestRequest restRequest = new RestRequest(request_resource, Method.Post);
+            restRequest.AddHeader("Authorization", authorization);
+            restRequest.AddHeader("Content-Type", "application/json");
+
+            restRequest.AddJsonBody<string>(networkRequestObject);
+
+            RestResponse response = restClient.Execute(restRequest);
+            if (response.IsSuccessStatusCode && response.Content != null)
+            {
+                return response.Content;
+            }
+            return "{}";
+        }
+    }
+}
